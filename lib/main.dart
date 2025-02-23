29c1e441d163286f7f6d762cb2c4d39cbd3e8871
niyonkuru2 <niyonkurusam5@gmail.com>
2025-02-23 15:41:27 +0200

    debugging

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shrine/home.dart';
+import 'package:shrine/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Shrine',
-      home: const HomePage(),
+      home: const LoginPage(),
     );
   }
 }
